b05f5f79193feda20394654176ab6a923fc8fa7f
John Smith <ferisakuragi@gmail.com>
2020-09-15 14:43:14 +0700

    Initial commit

---
+++ controllers/adminController.js
@@ -0,0 +1,39 @@
+const Category = require('../models/Category')
+
+module.exports = {
+  viewDashboard: (req, res) => {
+    res.render("admin/dashboard/view_dashboard");
+  },
+
+  viewCategory: async (req, res) => {
+    const category = await Category.find();
+    // console.log(category);
+    res.render("admin/category/view_category", {
+      category
+    });
+
+  },
+
+  addCategory: async (req, res) => {
+    const {
+      name
+    } = req.body;
+    // console.log(name);
+    await Category.create({
+      name
+    })
+    res.redirect('/admin/category')
+  },
+
+  viewBank: (req, res) => {
+    res.render("admin/bank/view_bank");
+  },
+
+  viewItem: (req, res) => {
+    res.render("admin/item/view_item");
+  },
+
+  viewBooking: (req, res) => {
+    res.render("admin/booking/view_booking");
+  },
+};
---
+++ routes/admin.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+
+const adminController = require("../controllers/adminController");
+
+router.get("/dashboard", adminController.viewDashboard);
+router.get("/category", adminController.viewCategory);
+router.get("/bank", adminController.viewBank);
+router.get("/item", adminController.viewItem);
+router.get("/booking", adminController.viewBooking);
+
+router.post('/category', adminController.addCategory)
+module.exports = router;
