0a02a6b73556ff2887bdab7278fed6d943928f60
John Smith <ferisakuragi@gmail.com>
2020-09-18 14:06:30 +0700

    menambahkan edit dan hapus data bank

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,7 @@
 const Category = require("../models/Category");
 const Bank = require("../models/Bank");
+const fs = require("fs-extra");
+const path = require("path");
 
 module.exports = {
   viewDashboard: (req, res) => {
@@ -73,14 +75,16 @@ module.exports = {
     }
   },
 
-  viewBank: (req, res) => {
+  viewBank: async (req, res) => {
     try {
+      const bank = await Bank.find();
       const alertMessage = req.flash("alertMessage");
       const alertStatus = req.flash("alertStatus");
       const alert = { message: alertMessage, status: alertStatus };
       res.render("admin/bank/view_bank", {
         title: "Staycation | Bank",
         alert,
+        bank,
       });
     } catch (error) {
       req.flash("alertMessage", `${error.message}`);
@@ -100,6 +104,55 @@ module.exports = {
       });
       req.flash("alertMessage", "Success add data bank");
       req.flash("alertStatus", "success");
+      res.redirect("/admin/bank");
+    } catch (error) {
+      req.flash("alertMessage", `${error.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/admin/bank");
+    }
+  },
+
+  editBank: async (req, res) => {
+    try {
+      const { id, name, nameBank, nomorRekening } = req.body;
+
+      const bank = await Bank.findOne({ _id: id });
+      // apabila tidak edit gambar
+      if (req.file == undefined) {
+        bank.name = name;
+        bank.nameBank = nameBank;
+        bank.nomorRekening = nomorRekening;
+        await bank.save();
+        req.flash("alertMessage", "Success update data bank");
+        req.flash("alertStatus", "success");
+        res.redirect("/admin/bank");
+      } else {
+        // apabila ingin edit gambar
+        await fs.unlink(path.join(`public/${bank.imageUrl}`));
+        bank.name = name;
+        bank.nameBank = nameBank;
+        bank.nomorRekening = nomorRekening;
+        bank.imageUrl = `images/${req.file.filename}`;
+        await bank.save();
+        req.flash("alertMessage", "Success update data bank");
+        req.flash("alertStatus", "success");
+        res.redirect("/admin/bank");
+      }
+    } catch (error) {
+      req.flash("alertMessage", `${error.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/admin/bank");
+    }
+  },
+
+  deleteBank: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const bank = await Bank.findOne({ _id: id });
+      await fs.unlink(path.join(`public/${bank.imageUrl}`));
+      await bank.remove();
+      req.flash("alertMessage", "Success delete data bank");
+      req.flash("alertStatus", "warning");
       res.redirect("/admin/bank");
     } catch (error) {
       req.flash("alertMessage", `${error.message}`);
--- routes/admin.js
+++ routes/admin.js
@@ -2,21 +2,23 @@ const router = require("express").Router();
 const adminController = require("../controllers/adminController");
 const { upload } = require("../middlewares/multer");
 
-// router untuk view ke mana saja
+// router atau endpoint untuk view ke mana saja
 router.get("/dashboard", adminController.viewDashboard);
 router.get("/category", adminController.viewCategory);
 router.get("/bank", adminController.viewBank);
 router.get("/item", adminController.viewItem);
 router.get("/booking", adminController.viewBooking);
 
-// router untuk menambah data
+// router atau endpoint untuk menambah data
 router.post("/category", adminController.addCategory);
 router.post("/bank", upload, adminController.addBank);
 
-// router untuk mengedit data
+// router atau endpoint untuk mengedit data
 router.put("/category", adminController.editCategory);
+router.put("/bank", upload, adminController.editBank);
 
-// router untuk menghapus data
+// router atau endpoint untuk menghapus data
 router.delete("/category/:id", adminController.deleteCategory);
+router.delete("/bank/:id", adminController.deleteBank);
 
 module.exports = router;
