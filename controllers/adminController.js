1e616de8d2d8460331a9f1780a2ccd35961a4783
John Smith <ferisakuragi@gmail.com>
2020-09-25 10:08:44 +0700

    menambahkan crud activity pada detail item

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,6 +2,7 @@ const Category = require("../models/Category");
 const Bank = require("../models/Bank");
 const Item = require("../models/Item");
 const Feature = require("../models/Feature");
+const Activity = require("../models/Activity");
 const Image = require("../models/Image");
 const fs = require("fs-extra");
 const path = require("path");
@@ -358,13 +359,16 @@ module.exports = {
       const alertMessage = req.flash("alertMessage");
       const alertStatus = req.flash("alertStatus");
       const alert = { message: alertMessage, status: alertStatus };
+
       const feature = await Feature.find({ itemId: itemId });
+      const activity = await Activity.find({ itemId: itemId });
 
       res.render("admin/item/detail_item/view_detail_item", {
         title: "Staycation | Detail Item",
         alert,
         itemId,
         feature,
+        activity,
       });
     } catch (error) {
       req.flash("alertMessage", `${error.message}`);
@@ -448,6 +452,90 @@ module.exports = {
       await fs.unlink(path.join(`public/${feature.imageUrl}`));
       await feature.remove();
       req.flash("alertMessage", "Success delete data feature");
+      req.flash("alertStatus", "warning");
+      res.redirect(`/admin/item/show-detail-item/${itemId}`);
+    } catch (error) {
+      req.flash("alertMessage", `${error.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect(`/admin/item/show-detail-item/${itemId}`);
+    }
+  },
+
+  addActivity: async (req, res) => {
+    const { name, type, itemId } = req.body;
+    try {
+      if (!req.file) {
+        req.flash("alertMessage", "Image not found");
+        req.flash("alertStatus", "danger");
+        res.redirect(`/admin/item/show-detail-item/${itemId}`);
+      }
+      const activity = await Activity.create({
+        name,
+        type,
+        itemId,
+        imageUrl: `images/${req.file.filename}`,
+      });
+
+      const item = await Item.findOne({ _id: itemId });
+      item.activityId.push({ _id: activity._id });
+      await item.save();
+
+      req.flash("alertMessage", "Success add data activity");
+      req.flash("alertStatus", "success");
+      res.redirect(`/admin/item/show-detail-item/${itemId}`);
+    } catch (error) {
+      req.flash("alertMessage", `${error.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect(`/admin/item/show-detail-item/${itemId}`);
+    }
+  },
+
+  editActivity: async (req, res) => {
+    const { id, name, type, itemId } = req.body;
+    try {
+      const activity = await Activity.findOne({ _id: id });
+      // apabila tidak edit gambar
+      if (req.file == undefined) {
+        activity.name = name;
+        activity.type = type;
+        await activity.save();
+        req.flash("alertMessage", "Success update data activity");
+        req.flash("alertStatus", "success");
+        res.redirect(`/admin/item/show-detail-item/${itemId}`);
+      } else {
+        // apabila ingin edit gambar
+        await fs.unlink(path.join(`public/${activity.imageUrl}`));
+        activity.name = name;
+        activity.type = type;
+        activity.imageUrl = `images/${req.file.filename}`;
+        await activity.save();
+        req.flash("alertMessage", "Success update data activity");
+        req.flash("alertStatus", "success");
+        res.redirect(`/admin/item/show-detail-item/${itemId}`);
+      }
+    } catch (error) {
+      req.flash("alertMessage", `${error.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect(`/admin/item/show-detail-item/${itemId}`);
+    }
+  },
+
+  deleteActivity: async (req, res) => {
+    const { id, itemId } = req.params;
+    try {
+      const activity = await Activity.findOne({ _id: id });
+      const item = await (await Item.findOne({ _id: itemId })).populate(
+        "activityId"
+      );
+      for (let i = 0; i < item.activityId.length; i++) {
+        if (item.activityId[i]._id.toString() === activity._id.toString()) {
+          item.activityId.pull({ _id: activity._id });
+          await item.save();
+        }
+      }
+      await fs.unlink(path.join(`public/${activity.imageUrl}`));
+      await activity.remove();
+      req.flash("alertMessage", "Success delete data activity");
       req.flash("alertStatus", "warning");
       res.redirect(`/admin/item/show-detail-item/${itemId}`);
     } catch (error) {
--- routes/admin.js
+++ routes/admin.js
@@ -16,18 +16,21 @@ router.post("/category", adminController.addCategory);
 router.post("/bank", uploadSingle, adminController.addBank);
 router.post("/item", uploadMultiple, adminController.addItem);
 router.post("/item/add/feature", uploadSingle, adminController.addFeature);
+router.post("/item/add/activity", uploadSingle, adminController.addActivity);
 
 // router atau endpoint untuk mengedit data
 router.put("/category", adminController.editCategory);
 router.put("/bank", uploadSingle, adminController.editBank);
 router.put("/item/:id", uploadMultiple, adminController.editItem);
 router.put("/item/update/feature", uploadSingle, adminController.editFeature);
+router.put("/item/update/activity", uploadSingle, adminController.editActivity);
 
 // router atau endpoint untuk menghapus data
 router.delete("/category/:id", adminController.deleteCategory);
 router.delete("/bank/:id", adminController.deleteBank);
 router.delete("/item/:id/delete", adminController.deleteItem);
 router.delete("/item/:itemId/feature/:id", adminController.deleteFeature);
+router.delete("/item/:itemId/activity/:id", adminController.deleteActivity);
 
 // router atau endpoint detail item
 router.get("/item/show-detail-item/:itemId", adminController.viewDetailItem);
