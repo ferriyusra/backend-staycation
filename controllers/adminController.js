95fb059ca83bf472ab4f2bf883c20887ff06ca82
John Smith <ferisakuragi@gmail.com>
2020-09-26 09:19:57 +0700

    membuat halaman dashboard dengan menjumlahkan total member , item dan order

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -60,13 +60,21 @@ module.exports = {
     res.redirect("/admin/signin");
   },
 
-  viewDashboard: (req, res) => {
+  viewDashboard: async (req, res) => {
     try {
+      const member = await Member.find();
+      const booking = await Booking.find();
+      const item = await Item.find();
       res.render("admin/dashboard/view_dashboard", {
         title: "Staycation | Dashboard",
         user: req.session.user,
+        member,
+        booking,
+        item,
       });
-    } catch (error) {}
+    } catch (error) {
+      res.redirect("/admin/dashboard");
+    }
   },
 
   viewCategory: async (req, res) => {
