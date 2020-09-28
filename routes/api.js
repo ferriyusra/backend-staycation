65ccf54f5e11252a09b7c3704f646de4906a7acb
John Smith <ferisakuragi@gmail.com>
2020-09-28 10:47:55 +0700

    membut API detail page dan konfigruasi router untuk api detail page
    pusing heula

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,8 @@ const router = require("express").Router();
 const apiController = require("../controllers/apiController");
 // const { uploadSingle, uploadMultiple } = require("../middlewares/multer");
 
-// menggunakan middlewares auth untuk session login
+// router api landing page
 router.get("/landing-page", apiController.landingPage);
-
+// router api item detail
+router.get("/detail-page/:id", apiController.detailPage);
 module.exports = router;
